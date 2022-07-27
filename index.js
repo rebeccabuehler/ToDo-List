6d3303a72d442e84c7f9a7a0a72f5a9188930ee9
Rebecca Buehler <you@example.com>
2022-07-27 11:09:13 -0700

    setting initial values for the todo list

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 function App() {
+    const [todos, setTodos] = React.useState([
+        {
+            text: 'Learn React',
+            isComplete: false,
+        },
+        {
+            text: 'Meeting a friend for lunch',
+            isComplete: false,
+        },
+        {
+            text: 'Build ToDo App',
+            isComplete: false,
+        }
+    ]);
     return (<></>);
 }
 
