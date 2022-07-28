2911884d1ffd8822adaffce7ed7b59d59dfeb52d
Rebecca Buehler <you@example.com>
2022-07-28 16:37:04 -0700

    added a form js file to clean up code

---
+++ form.js
@@ -0,0 +1,21 @@
+function ToDoForm(addToDo) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add ToDo ..."
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
--- index.js
+++ index.js
@@ -1,48 +1,39 @@
 function App() {
-    const [todos, setTodos] = React.useState([
-        {
-            text: 'Learn React',
-            isComplete: false,
-        },
-        {
-            text: 'Meeting a friend for lunch',
-            isComplete: false,
-        },
-        {
-            text: 'Build ToDo App',
-            isComplete: false,
-        }
-    ]);
-    const [value, setValue] = React.useState('');
-    const handleSubmit = e => {
-        e.preventDefault();
-        if(!value) return;
-        const newToDos = [...todos, {text: value, isComplete: false}];
-        setTodos(newToDos);
-        setValue('');
-    };
-    const removeTodo = e => {
-        const index = Number(e.target.id);
-        let temp = [...todos];
-        temp.splice(index, 1);
-        setTodos(temp);
-    }
-    return (<>
-        {todos.map((todo, i) => <div key={i} id={i} onClick={removeTodo}>{todo.text}</div>)}
-        <form onSubmit={handleSubmit}>
-            <input
-            type="text"
-            className="input"
-            value={value}
-            placeholder="Add ToDo ..."
-            onChange={e => setValue(e.target.value)}
-            />
-        </form>
-    </>);
+  const [todos, setTodos] = React.useState([
+    {
+      text: "Learn React",
+      isComplete: false,
+    },
+    {
+      text: "Meeting a friend for lunch",
+      isComplete: false,
+    },
+    {
+      text: "Build ToDo App",
+      isComplete: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newToDos = [...todos, { text: value, isComplete: false }];
+    setTodos(newToDos);
+  };
+  const removeTodo = (e) => {
+    const index = Number(e.target.id);
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  };
+  return (
+    <>
+      {todos.map((todo, i) => (
+        <div key={i} id={i} onClick={removeTodo}>
+          {todo.text}
+        </div>
+      ))}
+      <ToDoForm addTodo={addTodo} />
+    </>
+  );
 }
 
-ReactDOM.render(
-    <App/>
-,
-document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
